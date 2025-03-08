96a1e6dfe2a970e1308a72f39ae0e97efb25b0ce
ajitsirg <ajittechanical@gmail.com>
2025-03-09 03:17:34 +0530

    views. routing,swagger

--- chat/urls.py
+++ chat/urls.py
@@ -1,7 +1,29 @@
 from django.urls import path
-from .views import MessageListCreateView, GroupChatListCreateView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import (
+    UserListView, UserDetailView,
+    MessageCreateView, MessageListView, MessageDetailView,
+    GroupChatCreateView, GroupChatListView, GroupChatDetailView
+)
+
+
 
 urlpatterns = [
-    path('messages/', MessageListCreateView.as_view(), name='messages'),
-    path('groups/', GroupChatListCreateView.as_view(), name='group_chats'),
+    # Authentication Endpoints
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    # User Endpoints
+    path('users/', UserListView.as_view(), name='user-list'),
+    path('users/me/', UserDetailView.as_view(), name='user-detail'),
+
+    # Message Endpoints
+    path('messages/', MessageCreateView.as_view(), name='message-create'),
+    path('messages/list/', MessageListView.as_view(), name='message-list'),
+    path('messages/<int:pk>/', MessageDetailView.as_view(), name='message-detail'),
+
+    # GroupChat Endpoints
+    path('groupchats/', GroupChatCreateView.as_view(), name='groupchat-create'),
+    path('groupchats/list/', GroupChatListView.as_view(), name='groupchat-list'),
+    path('groupchats/<int:pk>/', GroupChatDetailView.as_view(), name='groupchat-detail'),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,104 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from django.shortcuts import get_object_or_404
+from .models import CustomUser, Message, GroupChat
+from .serializers import CustomUserSerializer, MessageSerializer, GroupChatSerializer
 
-# Create your views here.
+
+# User Views
+class UserListView(generics.ListAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class UserDetailView(generics.RetrieveUpdateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
+
+
+# Message Views
+class MessageCreateView(generics.CreateAPIView):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(sender=self.request.user)
+
+
+class MessageListView(generics.ListAPIView):
+    serializer_class = MessageSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Message.objects.filter(sender=user) | Message.objects.filter(receiver=user)
+
+
+class MessageDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_update(self, serializer):
+        message = self.get_object()
+        if message.sender != self.request.user:
+            return Response({"detail": "You do not have permission to edit this message."}, status=status.HTTP_403_FORBIDDEN)
+        serializer.save()
+
+    def perform_destroy(self, instance):
+        user = self.request.user
+        if instance.sender != user and instance.receiver != user:
+            return Response({"detail": "You do not have permission to delete this message."}, status=status.HTTP_403_FORBIDDEN)
+        instance.delete()
+
+
+# GroupChat Views
+class GroupChatCreateView(generics.CreateAPIView):
+    queryset = GroupChat.objects.all()
+    serializer_class = GroupChatSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        group = serializer.save(admin=self.request.user)
+        group.members.add(self.request.user)
+
+
+class GroupChatListView(generics.ListAPIView):
+    serializer_class = GroupChatSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return GroupChat.objects.filter(members=self.request.user)
+
+
+class GroupChatDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = GroupChat.objects.all()
+    serializer_class = GroupChatSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_update(self, serializer):
+        group = self.get_object()
+        if group.admin != self.request.user:
+            return Response({"detail": "You do not have permission to update this group."}, status=status.HTTP_403_FORBIDDEN)
+        serializer.save()
+
+    def perform_destroy(self, instance):
+        if instance.admin != self.request.user:
+            return Response({"detail": "You do not have permission to delete this group."}, status=status.HTTP_403_FORBIDDEN)
+        instance.delete()
